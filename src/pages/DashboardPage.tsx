2a991e45e800db98a375b0b719d0c5775c014a9e
Jesper Persson <Jespe9103@gmail.com>
2025-05-12 22:45:00 +0200

    Update DashboardPage layout and styling

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FiClock, FiMapPin, FiUpload, FiHome, FiLogOut } from 'react-icons/fi';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import { auth } from '../config/firebase';
